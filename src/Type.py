a42b7ed106e29b918a643669b77402a831186b96
mahbodmajid <mahbod.majid@gmail.com>
2019-01-24 14:40:18 +0330

    IMPLEMENT Type

---
+++ src/Type.py
@@ -0,0 +1,10 @@
+class Type:
+    register_req = '1'
+    register_res = '2'
+    advertise_req = '3'
+    advertise_res = '4'
+    join = '5'
+    message = '6'
+    reunion_hello = '7'
+    reunion_back = '8'
+
