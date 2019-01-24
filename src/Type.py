5c0430dc11c1f19d317032e9e5fc5a22fa815332
mahbodmajid <mahbod.majid@gmail.com>
2019-01-24 14:47:53 +0330

    CHANGE Type to packet header types

--- src/Type.py
+++ src/Type.py
@@ -1,10 +1,7 @@
 class Type:
-    register_req = '1'
-    register_res = '2'
-    advertise_req = '3'
-    advertise_res = '4'
-    join = '5'
-    message = '6'
-    reunion_hello = '7'
-    reunion_back = '8'
+    register = '1'
+    advertise = '2'
+    join = '3'
+    message = '4'
+    reunion = '5'
 
